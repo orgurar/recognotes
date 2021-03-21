138a1564218c5f27ea39c066e7617e6cd6dc8c2a
Or Gur Arie <orgurar@gmail.com>
2021-03-21 23:57:57 +0200

    More work on fromend

--- frontend/src/components/AudioRecorderComponent.js
+++ frontend/src/components/AudioRecorderComponent.js
@@ -1,23 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+import Swal from "sweetalert2";
 
 import AudioReactRecorder, { RecordState } from "audio-react-recorder";
 
-import { Typography, Grid, Button, TextField } from "@material-ui/core";
+import {
+  Typography,
+  Grid,
+  Button,
+  TextField,
+  CircularProgress,
+  Badge,
+  IconButton,
+} from "@material-ui/core";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 
+import MicIcon from "@material-ui/icons/Mic";
+import MicOffIcon from "@material-ui/icons/MicOff";
+
 import { serverURL } from "../utils/serverInfo";
+
+import "./AudioRecorder.css";
 
 const useStyles = makeStyles({
   root: {
     marginTop: 10,
   },
   formButton: {
-    width: 100,
-    fontSize: 18,
-    backgroundColor: "rgb(245,211,114)",
-    color: "white",
-    borderRadius: 25,
+    backgroundColor: "white",
+    color: "black",
+    "&:hover": {
+      backgroundColor: "transparent",
+      color: "white",
+    },
+  },
+  recordButtonIcon: {
+    fontSize: 60,
+    padding: 10,
   },
   whiteColored: {
     color: "white",
@@ -29,6 +49,18 @@ const useStyles = makeStyles({
   },
   gridColItem: {
     marginTop: 15,
+  },
+  uiLoading: {
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  submitButton: {
+    color: "white",
+    position: "absolute",
+    right: 30,
+    top: 50,
   },
 });
 
@@ -46,10 +78,7 @@ const CssTextField = withStyles({
     "& .MuiOutlinedInput-root": {
       "& fieldset": {
         borderColor: "white",
-        borderRadius: 50,
-      },
-      "&:hover fieldset": {
-        borderColor: "white",
+        borderRadius: 50, // <div className="root" style={{ filter: "blur(0px)" }}>
       },
       "&.Mui-focused fieldset": {
         borderColor: "white",
@@ -58,14 +87,43 @@ const CssTextField = withStyles({
   },
 })(TextField);
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 function AudioRecorder(props) {
   const classes = useStyles();
+
+  const { width } = useWindowDimensions();
+
+  const [hasRecorded, setHasRecorded] = useState(false);
 
   const [recordState, setRecordState] = useState(null);
   const [wavfileBlob, setWavfileBlob] = useState({});
 
   const [isRecording, setIsRecording] = useState(false);
-  // const [uiLoading, setUiLoading] = useState(false);
+  const [uiLoading, setUiLoading] = useState(false);
 
   const [sampleRate, setSampleRate] = useState(44100);
   const [bpm, setBpm] = useState(120);
@@ -78,7 +136,14 @@ function AudioRecorder(props) {
 
   const stopRecording = () => {
     setRecordState(RecordState.STOP);
+    setHasRecorded(true);
     setIsRecording(false);
+    Swal.fire({
+      icon: "success",
+      title: "Recording Saved!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   };
 
   const saveRecording = (audioData) => {
@@ -89,7 +154,7 @@ function AudioRecorder(props) {
   const submitForm = (event) => {
     event.preventDefault();
 
-    // setUiLoading(true)
+    setUiLoading(true);
     setSheetsTitle("new title");
 
     // first of all, create the wav file from the blob
@@ -118,137 +183,133 @@ function AudioRecorder(props) {
         //Build a URL from the file
         const fileURL = URL.createObjectURL(file);
         //Open the URL on new Window
-        // setUiLoading(false);
+        setUiLoading(false);
         window.open(fileURL);
       })
       .catch((error) => {
         console.error(error);
-        // setUiLoading(false);
+        setUiLoading(false);
       });
   };
 
+  const getScreenSize = (screenPercentage) => {
+    return width * screenPercentage;
+  };
+
   return (
-    <Grid
-      container
-      spacing={2}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      className={classes.root}
-    >
-      <Grid item>
-        <Typography
-          variant="h1"
-          className={classes.whiteColored}
-          style={{ marginRight: 28, fontWeight: "bold" }}
-        >
-          <span style={{ color: "rgb(191,234,230)" }}>Recog</span>
-          <span style={{ color: "rgb(246,185,184)" }}>Notes</span>
-        </Typography>
-      </Grid>
-
-      <Grid
-        item
-        container
-        direction="column"
-        alignItems="center"
-        justify="center"
-      >
+    <div className="root">
+      {uiLoading ? (
+        <div className={classes.uiLoading}>
+          <CircularProgress size={100} />
+        </div>
+      ) : (
         <Grid
-          item
-          // style={{
-          //   position: "absolute",
-          //   right: 100,
-          //   top: 250,
-          // }}
-        >
-          <AudioReactRecorder
-            state={recordState}
-            onStop={saveRecording}
-            canvasHeight={150}
-            foregroundColor="rgb(190,226,222)"
-            backgroundColor="rgb(246,185,184)"
-          />
-        </Grid>
-        <Grid
-          item
-          // style={{
-          //   position: "absolute",
-          //   right: 300,
-          //   top: 410,
-          // }}
-        >
-          {isRecording ? (
-            <Button onClick={stopRecording} className={classes.formButton}>
-              Stop
-            </Button>
-          ) : (
-            <Button onClick={startRecording} className={classes.formButton}>
-              Start
-            </Button>
-          )}
-        </Grid>
-      </Grid>
-
-      <form onSubmit={submitForm} noValidate>
-        <Grid
-          item
           container
+          spacing={2}
           direction="column"
           alignItems="center"
           justify="center"
+          className={classes.root}
         >
-          <Grid
-            item
-            className={classes.gridColItem}
-            // style={{
-            //   position: "absolute",
-            //   left: 300,
-            //   top: 410,
-            // }}
-          >
-            <CssTextField
-              label="BPM"
-              variant="outlined"
-              value={bpm}
-              onChange={(event) => {
-                setBpm(event.target.value);
-              }}
-              InputProps={{
-                className: classes.whiteColored,
-              }}
-            />
+          <Grid item>
+            <Typography
+              variant="h1"
+              className={classes.whiteColored}
+              style={{ fontWeight: "bold" }}
+            >
+              RecogNotes
+            </Typography>
+            <Typography variant="h6" className={classes.whiteColored}>
+              Press <MicIcon style={{ fontSize: 20 }} /> to Start Recording
+            </Typography>
           </Grid>
+
+          <Button className={classes.submitButton}>Continue</Button>
 
           <Grid
             item
-            className={classes.gridColItem}
-            // style={{
-            //   position: "absolute",
-            //   left: 300,
-            //   top: 480,
-            // }}
+            container
+            direction="column"
+            alignItems="left"
+            justify="left"
           >
-            <CssTextField
-              label="Sample Rate"
-              variant="outlined"
-              value={sampleRate}
-              onChange={(event) => {
-                setSampleRate(event.target.value);
-              }}
-              InputProps={{
-                className: classes.whiteColored,
-              }}
-            />
+            <Grid item style={{ left: 0, marginTop: "10%" }}>
+              <AudioReactRecorder
+                state={recordState}
+                onStop={saveRecording}
+                canvasHeight={150}
+                canvasWidth={getScreenSize(0.64)}
+                foregroundColor="white"
+                backgroundColor="#191919"
+              />
+            </Grid>
           </Grid>
-          <Grid item className={classes.gridColItem}>
-            <Button type="submit" className={classes.formButton}>
-              Submit
-            </Button>
+
+          <Grid item>
+            {isRecording ? (
+              <IconButton
+                onClick={stopRecording}
+                className={classes.formButton}
+              >
+                <MicOffIcon className={classes.recordButtonIcon} />
+              </IconButton>
+            ) : (
+              <Badge variant="dot" color="primary" invisible={!hasRecorded}>
+                <IconButton
+                  onClick={startRecording}
+                  className={classes.formButton}
+                >
+                  <MicIcon className={classes.recordButtonIcon} />
+                </IconButton>
+              </Badge>
+            )}
           </Grid>
+
+          {/* <form onSubmit={submitForm} noValidate>
+            <Grid
+              item
+              container
+              direction="column"
+              alignItems="center"
+              justify="center"
+            >
+              <Grid item className={classes.gridColItem}>
+                <CssTextField
+                  label="BPM"
+                  variant="outlined"
+                  value={bpm}
+                  onChange={(event) => {
+                    setBpm(event.target.value);
+                  }}
+                  InputProps={{
+                    className: classes.whiteColored,
+                  }}
+                />
+              </Grid>
+
+              <Grid item className={classes.gridColItem}>
+                <CssTextField
+                  label="Sample Rate"
+                  variant="outlined"
+                  value={sampleRate}
+                  onChange={(event) => {
+                    setSampleRate(event.target.value);
+                  }}
+                  InputProps={{
+                    className: classes.whiteColored,
+                  }}
+                />
+              </Grid>
+              <Grid item className={classes.gridColItem}>
+                <Button type="submit" className={classes.formButton}>
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form> */}
         </Grid>
-      </form>
-    </Grid>
+      )}
+    </div>
   );
 }
 
---
+++ frontend/src/components/test.js
@@ -0,0 +1,3 @@
+export default function Test(props) {
+  return <div></div>;
+}
