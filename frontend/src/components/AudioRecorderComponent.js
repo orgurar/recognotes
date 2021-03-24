020e437cbdfabb8fa052ac114797870d70740b34
Or Gur Arie <orgurar@gmail.com>
2021-03-24 20:33:46 +0200

    Added title and submit dialog to frontend

--- frontend/src/components/AudioRecorderComponent.js
+++ frontend/src/components/AudioRecorderComponent.js
@@ -1,27 +1,20 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { Document } from "react-pdf";
 
 import Swal from "sweetalert2";
 
 import AudioReactRecorder, { RecordState } from "audio-react-recorder";
 
-import {
-  Typography,
-  Grid,
-  Button,
-  TextField,
-  CircularProgress,
-  Badge,
-  IconButton,
-} from "@material-ui/core";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
+import AudioSubmitDialogComponent from "./AudioSubmitDialogComponent";
+
+import { Typography, Grid, Button, Badge, IconButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import MicIcon from "@material-ui/icons/Mic";
 import MicOffIcon from "@material-ui/icons/MicOff";
 
-import { serverURL } from "../utils/serverInfo";
-
 import "./AudioRecorder.css";
+import ArrowRightAlt from "@material-ui/icons/ArrowRightAlt";
 
 const useStyles = makeStyles({
   root: {
@@ -57,35 +50,19 @@ const useStyles = makeStyles({
     transform: "translate(-50%, -50%)",
   },
   submitButton: {
+    fontSize: 18,
     color: "white",
+    borderRadius: 15,
     position: "absolute",
-    right: 30,
-    top: 50,
+    right: 40,
+    top: 55,
+    padding: "5px 15px",
+    "&:hover": {
+      color: "black",
+      backgroundColor: "white",
+    },
   },
 });
-
-const CssTextField = withStyles({
-  root: {
-    "& label": {
-      color: "white",
-    },
-    "& label.Mui-focused": {
-      color: "white",
-    },
-    "& .MuiInput-underline:after": {
-      borderBottomColor: "white",
-    },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "white",
-        borderRadius: 50, // <div className="root" style={{ filter: "blur(0px)" }}>
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "white",
-      },
-    },
-  },
-})(TextField);
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -123,11 +100,14 @@ function AudioRecorder(props) {
   const [wavfileBlob, setWavfileBlob] = useState({});
 
   const [isRecording, setIsRecording] = useState(false);
-  const [uiLoading, setUiLoading] = useState(false);
 
   const [sampleRate, setSampleRate] = useState(44100);
   const [bpm, setBpm] = useState(120);
-  const [sheetsTitle, setSheetsTitle] = useState("");
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const [pdfLink, setPdfLink] = useState("");
+  const [pdfReady, setPdfReady] = useState(false);
 
   const startRecording = () => {
     setRecordState(RecordState.START);
@@ -149,60 +129,24 @@ function AudioRecorder(props) {
   const saveRecording = (audioData) => {
     //audioData contains blob and blobUrl
     setWavfileBlob(audioData);
-  };
-
-  const submitForm = (event) => {
-    event.preventDefault();
-
-    setUiLoading(true);
-    setSheetsTitle("new title");
-
-    // first of all, create the wav file from the blob
-    const fileName = Math.random().toString(36).substring(6) + "_name.wav"; // random
-    const wavFile = new File([wavfileBlob.blob], fileName);
-
-    // creating form data to contain file
-    const formData = new FormData();
-
-    // attach wav file to form data
-    formData.append("file", wavFile);
-
-    // strings are easier to send and recieve via form data
-    const fileDataString = `{
-      "sample_rate": ${sampleRate},
-      "bpm": ${bpm},
-      "sheets_title": "${sheetsTitle}"
-    }`;
-    formData.append("file_data", fileDataString);
-
-    axios
-      .post(`${serverURL}/proccess_audio`, formData, { responseType: "blob" })
-      .then((response) => {
-        // save response as PDF blob
-        const file = new Blob([response.data], { type: "application/pdf" });
-        //Build a URL from the file
-        const fileURL = URL.createObjectURL(file);
-        //Open the URL on new Window
-        setUiLoading(false);
-        window.open(fileURL);
-      })
-      .catch((error) => {
-        console.error(error);
-        setUiLoading(false);
-      });
+    setSampleRate(44100);
+    setBpm(120);
   };
 
   const getScreenSize = (screenPercentage) => {
     return width * screenPercentage;
   };
 
+  const onLinkReady = (pdfLink) => {
+    setDialogOpen(false);
+    setPdfLink(pdfLink);
+    setPdfReady(true);
+    console.log("LINK", pdfLink);
+  };
+
   return (
     <div className="root">
-      {uiLoading ? (
-        <div className={classes.uiLoading}>
-          <CircularProgress size={100} />
-        </div>
-      ) : (
+      <>
         <Grid
           container
           spacing={2}
@@ -224,7 +168,13 @@ function AudioRecorder(props) {
             </Typography>
           </Grid>
 
-          <Button className={classes.submitButton}>Continue</Button>
+          <Button
+            className={classes.submitButton}
+            endIcon={<ArrowRightAlt style={{ fontSize: 30, marginTop: -4 }} />}
+            onClick={() => setDialogOpen(true)}
+          >
+            I'm Ready
+          </Button>
 
           <Grid
             item
@@ -264,51 +214,21 @@ function AudioRecorder(props) {
               </Badge>
             )}
           </Grid>
-
-          {/* <form onSubmit={submitForm} noValidate>
-            <Grid
-              item
-              container
-              direction="column"
-              alignItems="center"
-              justify="center"
-            >
-              <Grid item className={classes.gridColItem}>
-                <CssTextField
-                  label="BPM"
-                  variant="outlined"
-                  value={bpm}
-                  onChange={(event) => {
-                    setBpm(event.target.value);
-                  }}
-                  InputProps={{
-                    className: classes.whiteColored,
-                  }}
-                />
-              </Grid>
-
-              <Grid item className={classes.gridColItem}>
-                <CssTextField
-                  label="Sample Rate"
-                  variant="outlined"
-                  value={sampleRate}
-                  onChange={(event) => {
-                    setSampleRate(event.target.value);
-                  }}
-                  InputProps={{
-                    className: classes.whiteColored,
-                  }}
-                />
-              </Grid>
-              <Grid item className={classes.gridColItem}>
-                <Button type="submit" className={classes.formButton}>
-                  Submit
-                </Button>
-              </Grid>
-            </Grid>
-          </form> */}
         </Grid>
-      )}
+
+        <AudioSubmitDialogComponent
+          open={dialogOpen}
+          onClose={() => {
+            setDialogOpen(false);
+          }}
+          bpm={bpm}
+          sampleRate={sampleRate}
+          wavfileBlob={wavfileBlob}
+          onLinkReady={onLinkReady}
+        />
+
+        {pdfReady && <Document file={pdfLink} />}
+      </>
     </div>
   );
 }
